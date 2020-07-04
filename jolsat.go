91af394853331b54edf3a2dc3c01f3547f536aaa
Jonathan Matthews <contact@jpluscplusm.com>
2020-07-04 23:12:41 +0100

    WIP: adding basic field list parsing

--- jolsat.go
+++ jolsat.go
@@ -9,10 +9,12 @@ import (
 
 func main() {
 	delimiter := flag.String("d", "\t", "Word delimiter")
+	//fieldSpec := flag.String("f", "1-", "Field list")
 	flag.Parse()
 	if _, err := io.Copy(os.Stdout, os.Stdin); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 	fmt.Fprintln(os.Stderr, delimiter)
+	//fmt.Fprintln(os.Stderr, fieldSpec)
 }
